65c545e1b6949ad2ea41a8371f847c3088d51668
Akash Kriplani <aakash23kriplani@gmail.com>
2022-09-24 00:22:56 +0530

    Add base code to compodoc poc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact.component';
+import { DashboardComponent } from './components/dashboard.component';
+import { HomeComponent } from './components/home.component';
+import { ProfileComponent } from './components/profile.component';
+import { SettingsComponent } from './components/settings.component';
 
-const routes: Routes = [];
+const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
+import { ProfileComponent } from './components/profile.component';
+import { DashboardComponent } from './components/dashboard.component';
+import { ContactComponent } from './components/contact.component';
+import { SettingsComponent } from './components/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    DashboardComponent,
+    ContactComponent,
+    SettingsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
